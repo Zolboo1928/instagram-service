1f4d4101914c1abc7f8dc76aebd3b0005021823e
Zolboo1928 <zolbootulgaa1928@gmail.com>
2024-12-05 15:29:35 +0800

    cors

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const connectMongoose = require("./controller/connectMongoose")
 const userRoute = require("./routes/userRoute")
 const postRoute = require("./routes/postRoute")
 const dotenv = require("dotenv");
+const cors = require("cors")
 dotenv.config();
 
 const Port = 8000
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 connectMongoose()
 app.use("/user",userRoute)
