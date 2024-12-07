5177bb879aa5389ea5028aaba486a22a04abd04d
Zolboo1928 <zolbootulgaa1928@gmail.com>
2024-12-07 12:22:17 +0800

    m

--- models/postModel.js
+++ models/postModel.js
@@ -3,7 +3,7 @@ const postSchema = new Schema(
   {
     title: { type: String, required: true },
     postedUserImage: { type: String, required: true },
-    userId: { type: mongoose.Types.ObjectId, required: true },
+    userId: { type: mongoose.Types.ObjectId, required: true,ref:"users" },
     comments: [{ type: mongoose.Types.ObjectId, ref: "comments" }],
     likedUsers: [{ type: mongoose.Types.ObjectId, ref: "users" }],
   },
