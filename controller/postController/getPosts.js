1f13f42ecdc28a7c0388ba2cb4195b5522f87576
Zolboo1928 <zolbootulgaa1928@gmail.com>
2024-12-10 15:12:42 +0800

    m

--- controller/postController/getPosts.js
+++ controller/postController/getPosts.js
@@ -3,8 +3,7 @@ const postModel = require("../../models/postModel")
 const getPosts = async (req,res) =>{
     try {
         const Posts = await postModel.find().populate("userId");
-        console.log("getting data")
-        res.send(Posts)
+        res.send("working")
     } catch (error) {
         res.send(error)
     }
