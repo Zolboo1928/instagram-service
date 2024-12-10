c7a69c851feea515e34045ef4491dfb24c55c535
Zolboo1928 <zolbootulgaa1928@gmail.com>
2024-12-10 14:53:34 +0800

    m

--- models/postModel.js
+++ models/postModel.js
@@ -2,8 +2,9 @@ const { mongoose, Schema } = require("mongoose");
 const postSchema = new Schema(
   {
     title: { type: String, required: true },
+    postImage:{type: String },
     postedUserImage: { type: String, required: true },
-    userId: { type: mongoose.Types.ObjectId, required: true,ref:"users" },
+    userId: { type: mongoose.Types.ObjectId, required: true,ref:"users  " },
     comments: [{ type: mongoose.Types.ObjectId, ref: "comments" }],
     likedUsers: [{ type: mongoose.Types.ObjectId, ref: "users" }],
   },
