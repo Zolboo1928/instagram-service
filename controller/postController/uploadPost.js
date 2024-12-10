c3e49201fcb94527c3c4199dd7fafaad4094bada
Zolboo1928 <zolbootulgaa1928@gmail.com>
2024-12-10 17:28:12 +0800

    m

--- controller/postController/uploadPost.js
+++ controller/postController/uploadPost.js
@@ -4,12 +4,13 @@ const userModel = require("../../models/userModel");
 
 
 const uploadPost = async (req,res)=>{
-    const { title, postedUserImage, userId } = req.body;
+    const { title, postedUserImage, userId, postImage } = req.body;
     try {
         const postmodelRes  = await postModel.create({
         title,
         postedUserImage,
         userId,
+        postImage,
         })
         await userModel.findByIdAndUpdate(userId,{
             $push:{
