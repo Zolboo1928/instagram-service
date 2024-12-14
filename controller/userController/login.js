efdbcb2c048ddba2a496f2c4eb959973ef877389
Zolboo1928 <zolbootulgaa1928@gmail.com>
2024-12-14 12:03:10 +0800

    m

--- controller/userController/login.js
+++ controller/userController/login.js
@@ -1,5 +1,6 @@
 const userModel = require("../../models/userModel");
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 const login = async (req,res)=>{
        const { password, email } = req.body;                                     
@@ -7,15 +8,27 @@ try {
            const user = await userModel.findOne({
             email
            })
-           bcrypt.compare(password, user.password, (err , isMatch)=>{
+           bcrypt.compare(password, user.password, async(err , isMatch)=>{
                if(err){
                 res.send(err)
             } else if (isMatch){
-                res.send(user)
+                const infoInsideLoginToken = {
+                  _id: user._id,
+                  userName: user.userName,
+                  password: user.password,
+                  email: user.email,
+                  profileImage: user.profileImage,
+                    posts: user.posts,
+                      following: user.following,
+                      followers: user.followers,
+                };
+                const loginToken = await jwt.sign(infoInsideLoginToken, process.env.JWT_SECRET, {expiresIn:"24h"});
+                res.send({infoInsideLoginToken,loginToken})
             } else {
                 res.send("didn't match")
             }
            })
+            
 } catch (error) {
     res.send(error)
 }
