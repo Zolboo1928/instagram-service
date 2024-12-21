c3d76b24a79a7c7ac1307554c356f467729b8667
Zolboo1928 <zolbootulgaa1928@gmail.com>
2024-12-21 10:45:22 +0800

    m

--- controller/postController/getPostById.js
+++ controller/postController/getPostById.js
@@ -4,7 +4,12 @@ const postModel = require("../../models/postModel");
 const getPostById = async(req,res)=>{
     try {
         const {postId }= req.params
-        const post = await postModel.findById(postId).populate("comments");
+        const post = await postModel.findById(postId).populate({
+            path: "comments",
+            populate:{
+                path: "userId"
+            }
+        });
         res.send(post)
     } catch (error) {
         res.send(error)
