0cc85f674e11bcde1f6b67c0b1a08dcf8310693a
Zolboo1928 <zolbootulgaa1928@gmail.com>
2025-01-11 12:28:51 +0800

    M

--- controller/postController/getCommentsOfPost.js
+++ controller/postController/getCommentsOfPost.js
@@ -1,9 +1,15 @@
+const { populate } = require("dotenv");
 const postModel = require("../../models/postModel");
 
 const getCommentsOfPost = async (req, res) => {
   try {
     const { postId } = req.params;
-    const post = await postModel.findById(postId).populate("comments");
+    const post = await postModel.findById(postId).populate({
+        path:"comments",
+        populate:{
+            path:"userId"
+        }
+    });
     res.send(post);
   } catch (error) {
     res.send(error);
